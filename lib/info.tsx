b9ec409a357a1841512f918ffd1feb214c6330b2
Nathaniel Nemenzo <natnemenzo1@gmail.com>
2023-03-15 02:01:48 -0500

    small change to homepage

--- lib/info.tsx
+++ lib/info.tsx
@@ -12,7 +12,7 @@ export const about = () => {
 export const bio = () => {
   return (
     <>
-      Previously, I've interned at Amazon and NASA. I've also spent time in the Learning Agents Research Group at UT Austin. Outside of school and work, I'm also interested in urban environments and architecture. <br/><br/>
+      Previously, I've interned at Amazon and currently I'm at NASA doing research on blockchain-based storage for satellite observation data. I've also spent time in the Learning Agents Research Group at UT Austin. Outside of school and work, I'm also interested in urban environments and architecture. <br/><br/>
 
       This website is built using Next.js, deployed with Vercel and is based off of a template by Lee Robinson. You can find it <b><a href='https://github.com/leerob/leerob.io'>here.</a></b>
     </>
